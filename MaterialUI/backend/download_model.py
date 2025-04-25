d6cf00ded5d57376cde7351e074f2b02068869c6
bhavinkmokariya <bhavinkmokariya@gmail.com>
2025-04-25 21:57:44 +0530

    delete_backend_push

--- MaterialUI/backend/download_model.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoModel, AutoTokenizer
-
-model = AutoModel.from_pretrained("BAAI/BAAI/bge-base-en-v1.5", cache_dir="./BAAI-bge-base-en-v1.5")
-tokenizer = AutoTokenizer.from_pretrained("BAAI/BAAI/bge-base-en-v1.5", cache_dir="./BAAI-bge-base-en-v1.5")
